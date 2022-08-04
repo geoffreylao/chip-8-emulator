0c9f1c61a88a2f25057d36f1add5dd53754c4f77
geoffreylao <geoffreylao@outlook.com>
2022-08-04 16:23:17 -0300

    complete emulator

--- src/scripts/chip8.ts
+++ src/scripts/chip8.ts
@@ -1,36 +1,44 @@
-import Renderer from "./renderer";
-import Keyboard from "./keyboard";
-import Speaker from "./speaker";
+import Renderer from './renderer';
+import Keyboard from './keyboard';
+import Speaker from './speaker';
+import CPU from './cpu';
 
 const renderer = new Renderer(10);
 const keyboard = new Keyboard();
-const speaker = new Speaker()
+const speaker = new Speaker();
+const cpu = new CPU(renderer, keyboard, speaker);
 
 let loop: number;
 
-let fps = 60, fpsInterval: number, startTime, now, then: number, elapsed;
+let fps = 60,
+  fpsInterval: number,
+  startTime,
+  now,
+  then: number,
+  elapsed;
 
-function init(){
-  fpsInterval = 1000/fps;
+function init() {
+  fpsInterval = 1000 / fps;
   then = Date.now();
   startTime = then;
 
-  renderer.testRender();
-  renderer.render()
+  // renderer.testRender();
+  // renderer.render();
 
+  cpu.loadSpritesIntoMemory();
+  cpu.loadRom('/BLINKY');
   loop = requestAnimationFrame(step);
-  console.log(loop)
 }
 
-function step(){
+function step() {
   now = Date.now();
   elapsed = now - then;
 
-  if (elapsed > fpsInterval){
-
+  if (elapsed > fpsInterval) {
+    cpu.cycle();
   }
 
   loop = requestAnimationFrame(step);
 }
 
-init()
+init();
